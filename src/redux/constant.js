60504fcba6ecd529cd595b80c0515e5962996448
Nguyen Hai Long <long.nh@ascendcorp.com>
2019-09-30 00:27:43 +0700

    update hosting

--- src/redux/constant.js
+++ src/redux/constant.js
@@ -1,4 +1,4 @@
 export const server = {
-    // host: "https://procon2019-api.herokuapp.com",
-    host: "http://192.168.100.14:1234"
+    host: "https://procon2019-api.herokuapp.com",
+    // host: "http://192.168.100.14:1234"
 };
